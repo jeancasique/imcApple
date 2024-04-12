c7497b18634f3fd3f7e3914cda6c9b137fe7c7f3
Jean Paul Casique Hernandez <128648632+jeancasique@users.noreply.github.com>
2024-04-12 14:07:14 +0200

    commit viernes

---
+++ Exercises/TaskApp/NotaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NotaViewController.swift
+//  Exercises
+//
+//  Created by Mañanas on 12/4/24.
+//
+
+import UIKit
+
+class NotaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
