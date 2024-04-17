1d76818df813a58a0be5830caaa278d53bd75df4
Jean Paul Casique Hernandez <128648632+jeancasique@users.noreply.github.com>
2024-04-17 14:01:52 +0200

    commit 17 abril

---
+++ Exercises/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Exercises
+//
+//  Created by Mañanas on 17/4/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ Exercises/Login/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  Registro.swift
+//  Exercises
+//
+//  Created by Mañanas on 17/4/24.
+//
+
+import SwiftUI
+
+struct Registro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Registro()
+}
